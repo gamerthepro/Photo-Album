01bafb4b2e86f44978ceb0eb53d29da98bb0d7cd
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-06 12:52:42 +0300

    add component ImagePopupForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Main from './Main';
+import ImagePopupForm from './ImagePopupForm';
 
 function App() {
 	return (
 		<>
 		<Main/>
+		<ImagePopupForm/>
 		</>
 	)
 }
---
+++ src/components/ImagePopupForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function ImagePopupForm(props) {
+
+	return (
+		<div className={`popup popup_type_image ${props.card.link && 'popup__open'}`}>
+			<figure className="popup__container popup__container_image">
+				<button className="popup__close popup__close_image" type="button" aria-label="Закрыть" onClick={props.onClose}></button>
+				<img className="popup__img" src={props.card.link} alt={`Изображение места: ${props.card.name}`}/>
+				<figcaption className="popup__caption">{props.card.name}</figcaption>
+			</figure>
+		</div>
+	);
+}
+
+export default ImagePopupForm;
