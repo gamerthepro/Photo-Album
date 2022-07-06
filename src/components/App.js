02e8eeff1ffb6005ff9406edad6ec2e996ec9f07
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-06 13:38:47 +0300

    add component ImagePopupForm

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Main from './Main';
-import ImagePopupForm from './ImagePopupForm';
+// import ImagePopupForm from './ImagePopupForm';
 
 function App() {
 	return (
