43ccd233d289e31336366ee564371be5facd5d1c
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-07 20:29:17 +0300

    add like + React hook(useState)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 function Card(props) {
+	const [isAdd, setIsAdd] = React.useState(false);
+
+	const onClickLike = () => setIsAdd(!isAdd);
+	
 	return(
 		<section className='element-cards'>
 			<li className='element'>
 				<img className='element__image' src={props.link} alt='картинка'/>
-				<div className='element__like element__like_off'></div>
+				<div onClick={onClickLike} className={isAdd ? 'element__like element__like_on' : 'element__like element__like_off'}></div>
 			</li>
 		</section>
 	)
