13a4b60eb3bc25c32efc81181f824b9e4a3106f7
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-08 15:55:59 +0300

    add fetch

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import Card from './Card';
-import { initialCards } from './utils/Initial-сards'
+// import { initialCards } from './utils/Initial-сards'
 
 function Main(props) {
+	const [initialCards, isInitialCards] = React.useState([])
+	React.useEffect(() => {
+		fetch("https://62c81bb08c90491c2caeccc3.mockapi.io/initialCards")
+			.then((res) => {
+				return res.json();
+			})
+			.then((json) =>{
+				isInitialCards(json)
+			});
+	}, []);
 	return (
 		<body className='page'>
 			<div className='page__container'>
