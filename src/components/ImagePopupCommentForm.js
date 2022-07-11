9fc4825135228c9d16f7107a29519e338b4af9e8
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-11 18:23:38 +0300

    add html and css block coment

--- src/components/ImagePopupCommentForm.js
+++ src/components/ImagePopupCommentForm.js
@@ -11,16 +11,30 @@ function ImagePopupCommentForm({onClose, items}) {
 	return (
 		<div className= "popup popup_type_image popup__open">
 			{items.map((items) => (
-				<figure key={items.link} className="popup__container popup__container_image">
+				<div key={items.link} className="popup__container popup__container_image">
 					<button className="popup__close popup__close_image" type="button" aria-label="Закрыть" 
 					//вешаем слушетель на кнопку который вызывает функцию onClose из props она в свою очередь закрывает popup
 					onClick={onClose}></button>
 					<img className="popup__img" src={items.link} alt={`Изображение места: ${items.link}`}/>
 					<div className="popup__comment">
 						<title className="popup__title">{addComment}</title>
-						<input className="popup__input" onChange={onChangeComment}></input>
+						<input className="popup__input" 
+						type="text" 
+						minLength="0"
+						maxLength="500"
+						autoComplete="off"
+						name="Comment"
+						placeholder="добавить коментарий"
+						noValidate
+						required
+						onChange={onChangeComment}>
+						</input>
+						<div className="popup__buttons">
+							<button className="popup__button popup__button_cancel">отменить</button>
+							<button className="popup__button popup__button_add">добавить</button>
+						</div>
 					</div>
-				</figure>
+				</div>
 			))}
 		</div>
 	);
