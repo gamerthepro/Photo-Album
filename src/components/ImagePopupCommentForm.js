2df015c480d0bd0082000eac5b1152f09fbca34a
Artem Polonnikov <gamer.thepro@yandex.ru>
2022-07-16 19:00:55 +0300

    присвоил ключ комментарию и отправил на сервер

--- src/components/ImagePopupCommentForm.js
+++ src/components/ImagePopupCommentForm.js
@@ -1,36 +1,59 @@
 import React from 'react';
 
 function ImagePopupCommentForm({onClose, items}) {
-	//при помощи хука передаем данные из input в title
-	const [showComment, setShowComment] = React.useState('')
+	//При помощи этого хука передаём данные на сервер
+	const [onComment, setOnComment] = React.useState([])
 
+	//при помощи этого хука передаём данные(комментарии) в Doom
+	// const [showComments, setShowComments] = React.useState([])
 
-
-	// отправляем данные на сервер
 	React.useEffect(() => {
-		commitPush(showComment)
+		commitPush()// отправляем данные на сервер
+		// commitAdd()
 	}, []);
 
-	const commitPush = (showComment) => {
-		if (showComment !== '') {
+	
+
+	const commitPush = (onComment) => {
+		if (onComment !== '') {
 			fetch("https://62c81bb08c90491c2caeccc3.mockapi.io/comments", {
 				method: 'POST', // нужно указать метод запроса
-				headers: { "Content-Type": "application/json" },
-				body: JSON.stringify({comment: showComment})
+				headers: { "Content-Type": "application/json" },//указываем в каком виде досжна выглядеть структура данных
+				body: JSON.stringify(onComment)//переводим JSON формат в строку(так как всее данные по API запросам отправляються в JSON формате)
 			})
 			.then(res => {
-				res.json(showComment);
+				if (res.ok) {
+					return res.json(onComment);
+				} return console.log('данные не отправились на сервер')//улавливаем ошибку если данные не отправились на сервер
 			})
 		}
 	}
 
+	// const commitAdd = () => {
+	// 	fetch("https://62c81bb08c90491c2caeccc3.mockapi.io/comments")
+	// 		.then((res) => {
+	// 			return res.json();
+	// 		})
+	// 		.then((json) =>{
+	// 			setShowComments(json)
+	// 			console.log(json)
+	// 		});
+	// }
+
+	//слушатель кнопки "отправить" на клик
 	const buttonClick = (e) => {
 		e.preventDefault(e);
-		commitPush();
+		commitPush(onComment);
+		// commitAdd()
+		setOnComment([{}])//очищаем input (value)
 	}
 
+	//передаём value(данные из input) в 
 	const onInputChange = (e) => {
-		setShowComment(e.target.value)
+		setOnComment([{
+			comment: e.target.value,
+			key: e.target.id
+		}])
 	}
 
 	return (
@@ -41,10 +64,13 @@ function ImagePopupCommentForm({onClose, items}) {
 					//вешаем слушетель на кнопку который вызывает функцию onClose из props она в свою очередь закрывает popup
 					onClick={onClose}></button>
 					<img className="popup__img" src={items.link} alt={`Изображение места: ${items.link}`}/>
-					<div className="popup__comment">
-						<title className="popup__title"></title>
+					<div className="popup__form">
+						<ul className="popup__title">
+							<li className="popup__title"></li>
+						</ul>
 						<input className="popup__input" 
 						type="text" 
+						id={items.link}
 						minLength="0"
 						maxLength="500"
 						autoComplete="off"
@@ -52,6 +78,7 @@ function ImagePopupCommentForm({onClose, items}) {
 						placeholder="добавить коментарий"
 						noValidate
 						required
+						value={Object.comment}
 						onChange={onInputChange}
 						></input>
 						<div className="popup__buttons">
